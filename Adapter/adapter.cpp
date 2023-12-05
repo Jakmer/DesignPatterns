ebc8007b3968ddbbc094a4f3e57e415dba463655
Jakub <kubamerta13@gmail.com>
2023-12-05 19:48:36 +0100

    Expand example

--- Adapter/adapter.cpp
+++ Adapter/adapter.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <memory>
 
-/**  Suppose you have consol with HDMI cable and you need to plugin to VGA monitor
+/**  Suppose you have a console with HDMI cable and you need to plugin to VGA monitor
  *   You will need some kind of adapter to do this
  */
 
-// 1. Create two classes with different interfaces
+// 1. Create classes with different interfaces
 class HDMIsource
 {
     std::string output;
@@ -16,7 +16,7 @@ public:
         output = "This is HDMI display: ";
     }
 
-    virtual void hdmiDisplay(std::string &stream)
+    virtual void hdmiDisplay(std::string &stream, int type)
     {
         std::cout << output << stream << std::endl;
     }
@@ -31,20 +31,45 @@ public:
     {
         output = "This is VGA display: ";
     }
-    virtual void vgaDisplay(std::string &stream)
+    virtual void vgaDisplay(std::string &stream, int type)
+    {
+        std::cout << output << stream << std::endl;
+    }
+};
+
+class DisplayPortSource
+{
+    std::string output;
+
+public: 
+    DisplayPortSource()
+    {
+        output = "This is DisplayPort display: ";
+    }
+    virtual void displayPortDisplay(std::string &stream, int type)
     {
         std::cout << output << stream << std::endl;
     }
 };
 
 
-// 2. Create adapter class which will inherit from both classes
-class SourceAdapter : public HDMIsource, public AVGsource
+// 2. Create adapter class which will inherit from every classes
+class SourceAdapter : public HDMIsource, public AVGsource, public DisplayPortSource
 {
 public:
-    virtual void hdmiDisplay(std::string &stream)
+    virtual void hdmiDisplay(std::string &stream, int type) override
     {
-        vgaDisplay(stream);
+        switch (type)
+        {
+        case 1:
+            vgaDisplay(stream, type);
+            break;
+        case 2:
+            displayPortDisplay(stream, type);
+            break;
+        default:
+            break;
+        }
     }
 };
 
@@ -53,7 +78,8 @@ int main()
     std::string stream = "some digits";
     std::unique_ptr<HDMIsource> source = std::make_unique<SourceAdapter>();
 
-    source->hdmiDisplay(stream);
+    source->hdmiDisplay(stream,1);  // This is VGA display: some digits
+    source->hdmiDisplay(stream,2);  // This is DisplayPort display: some digits
 
     // 3. Now you can use HDMI interface to display on VGA monitor
 
